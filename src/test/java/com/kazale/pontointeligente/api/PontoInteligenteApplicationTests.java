4ceecda62600d1cbbb22703a537b29b755ba1f3d
AndreLps <andrelopes.spdia@gmail.com>
2021-05-24 18:19:58 -0300

    Adicionando e criando classes de teste com JUnit

--- src/test/java/com/kazale/pontointeligente/api/PontoInteligenteApplicationTests.java
+++ src/test/java/com/kazale/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -2,12 +2,15 @@ package com.kazale.pontointeligente.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PontoInteligenteApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
 
 }
