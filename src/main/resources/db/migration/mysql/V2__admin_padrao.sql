5979254f3e006a3245f0e4cbb2cbeed8e4775fd4
AndreLps <andrelopes.spdia@gmail.com>
2021-05-27 11:58:02 -0300

    Adicionado Heroku

--- src/main/resources/db/migration/mysql/V2__admin_padrao.sql
+++ src/main/resources/db/migration/mysql/V2__admin_padrao.sql
@@ -1,5 +1,14 @@
-INSERT INTO `empresa` (`id`, `cnpj`, `data_atualizacao`, `data_criacao`, `razao_social`) VALUES (NULL, '82198127000121', CURRENT_DATE(), CURRENT_DATE(), 'Splopes');
+--INSERT INTO `empresa` (`id`, `cnpj`, `data_atualizacao`, `data_criacao`, `razao_social`) VALUES (NULL, '82198127000121', CURRENT_DATE(), CURRENT_DATE(), 'Splopes');
 
-INSERT INTO `funcionario` (`id`, `cpf`, `data_atualizacao`, `data_criacao`, `email`, `nome`, `perfil`, `qtd_horas_almoco`, `qtd_horas_trabalho_dia`, `senha`,
-`valor_hora`, `empresa_id`) VALUES (NULL, '16248890935', CURRENT_DATE(), CURRENT_DATE(), 'admin@admin.com', 'ADMIN', 'ROLE_ADMIN', NULL, NULL,
-'$2a$10$OYYGj4v3CIi0m4nUSxW0Zu6GZIxtAq/WQkDcrCbl8Sja7N3rugRBS', NULL, (SELECT `id` FROM `empresa` WHERE `cnpj` = '82198127000121'));
+--INSERT INTO `funcionario` (`id`, `cpf`, `data_atualizacao`, `data_criacao`, `email`, `nome`, `perfil`, `qtd_horas_almoco`, `qtd_horas_trabalho_dia`, `senha`,
+--`valor_hora`, `empresa_id`) VALUES (NULL, '16248890935', CURRENT_DATE(), CURRENT_DATE(), 'admin@admin.com', 'ADMIN', 'ROLE_ADMIN', NULL, NULL,
+--'$2a$10$OYYGj4v3CIi0m4nUSxW0Zu6GZIxtAq/WQkDcrCbl8Sja7N3rugRBS', NULL, (SELECT `id` FROM `empresa` WHERE `cnpj` = '82198127000121'));
+
+INSERT INTO `empresa` (`id`, `cnpj`, `data_atualizacao`, `data_criacao`, `razao_social`)
+VALUES (NULL, '82198127000121', CURRENT_DATE(), CURRENT_DATE(), 'Kazale IT');
+
+INSERT INTO `funcionario` (`id`, `cpf`, `data_atualizacao`, `data_criacao`, `email`, `nome`,
+`perfil`, `qtd_horas_almoco`, `qtd_horas_trabalho_dia`, `senha`, `valor_hora`, `empresa_id`)
+VALUES (NULL, '16248890935', CURRENT_DATE(), CURRENT_DATE(), 'admin@kazale.com', 'ADMIN', 'ROLE_ADMIN', NULL, NULL,
+'$2a$06$xIvBeNRfS65L1N17I7JzgefzxEuLAL0Xk0wFAgIkoNqu9WD6rmp4m', NULL,
+(SELECT `id` FROM `empresa` WHERE `cnpj` = '82198127000121'));
